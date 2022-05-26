c130d8cf4a7ac69cd1470374827fbeb0088c0ff9
AdamFASHOO <AdamChapman1994@gmail.com>
2022-05-26 15:14:58 -0400

    Grand Chirpus Pizza lab finish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MenuItem } from './MenuItem';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ChirpusPizzaApp';
+
+  items: MenuItem[] = [
+    new MenuItem("Chicken Fingers", "dinner", 11.99),
+    new MenuItem("Pizza", "dinner", 11.99),
+    new MenuItem("Wings", "sides", 8.99),
+    new MenuItem("Breadsticks", "sides", 4.99),
+    new MenuItem("Ceasar Salad", "salads", 5.99),
+    new MenuItem("Cinnamon Roll", "desert", 8.99),
+  ];
 }
